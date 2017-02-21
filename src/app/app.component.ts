b4fe32754c1de727ce3042ea2d129b3c6dbda574
Leandro Garcia <leandro.garcias@gmail.com>
2017-02-21 18:22:01 -0300

    changes for android apk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,46 +10,46 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { AuthService } from '../providers/auth.service'
 
 @Component({
-    templateUrl: 'app.html'
+  templateUrl: 'app.html'
 })
 export class MyApp {
-    @ViewChild(Nav) nav: Nav;
-    rootPage;
-    pages: Array<{title: string, component: any, icon: string}>;
+  @ViewChild(Nav) nav: Nav;
+  rootPage;
+  pages: Array<{title: string, component: any, icon: string}>;
 
-    constructor(
-        private app: App,
-        private platform: Platform,
-        private _auth: AuthService,
-        private storage: Storage,
-        private config: Config
-    ){
-        platform.ready().then(() => {
-            StatusBar.styleDefault();
-            Splashscreen.hide();
-        });
+  constructor(
+    private app: App,
+    private platform: Platform,
+    private _auth: AuthService,
+    private storage: Storage,
+    private config: Config
+  ){
+    platform.ready().then(() => {
+      StatusBar.styleDefault();
+      Splashscreen.hide();
+    });
 
-        this.pages = [
-            // { title: 'About', component: AboutPage, icon: 'information-circle' }
-        ];
+    this.pages = [
+      // { title: 'About', component: AboutPage, icon: 'information-circle' }
+    ];
 
-        this.storage.get('idM-token').then((token) => {
-            if (token){
-                this.config.set('idM-token', token);
-                this.rootPage = TabsPage;
-            }
-            else
-                this.rootPage = AuthPage;
-        })
-    }
+    this.storage.get('idM-token').then((token) => {
+      if (token){
+        this.config.set('idM-token', token);
+        this.rootPage = TabsPage;
+      }
+      else
+        this.rootPage = AuthPage;
+    })
+  }
 
-    OpenPage(page) {
-        this.nav.setRoot(page.component);
-    }
+  OpenPage(page) {
+    this.nav.setRoot(page.component);
+  }
 
-    Logout(){
-        this._auth.Logout();
-        this.app.getRootNav().setRoot(AuthPage);
-    }
+  Logout(){
+    this._auth.Logout();
+    this.app.getRootNav().setRoot(AuthPage);
+  }
 
 }
